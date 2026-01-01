a2b8ab8b91a362655064ca47a0e2d12f65265915
Bhupinder <bhupinderk0511@gmail.com>
2026-01-01 13:14:46 +0530

    custom react js rendering structure and understanding

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Hello React !</h1>
+    </div>
+  )
+}
+
+const ReactElement = {
+  type: 'a',
+  props: {
+      href: 'https://www.google.com',
+      target: '_blank',
+      rel: 'noopener noreferrer'
+  },
+  children: 'Click Me To Visit Google!'
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+    // MyApp()
+
+    ReactElement
+
 )
